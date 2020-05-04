030eae7fac9b613c82ac8b30b1ac96796af8d3ed
yukimemi <yukimemi@gmail.com>
2020-05-05 00:11:02 +0900

    :zap: Add puppeteer plugin.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,42 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
+import * as puppeteer from "puppeteer";
 
 class Gtmt extends Command {
-  static description = 'Get my portfolio'
+  static description = "Get my portfolio";
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    force: flags.boolean({ char: "f" }),
+  };
+
+  headless = true;
+
+  slowMo = 0;
 
   async run() {
-    const {args, flags} = this.parse(Gtmt)
+    const { flags } = this.parse(Gtmt);
 
-    this.log(`hello from ./src/index.ts`)
+    this.log(`hello from ./src/index.ts`);
     if (flags.force) {
-      this.log(`you input --force`)
+      this.log(`you input --force`);
     }
+
+    const browser = await puppeteer.launch({
+      headless: this.headless,
+      slowMo: this.slowMo,
+    });
+    const page = await browser.newPage();
+    await page.goto("https://id.moneyforward.com/sign_in/email");
+    await page.screenshot({
+      path: "screenshot/moneyforward_sign_in.png",
+      fullPage: true,
+    });
+
+    await browser.close();
   }
 }
 
-export = Gtmt
+export = Gtmt;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,12 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from "@oclif/test";
 
-import cmd = require('../src')
+import cmd = require("../src");
 
-describe('gtmt', () => {
+describe("gtmt", () => {
   test
-  .stdout()
-  .do(() => cmd.run([]))
-  .it('runs gtmt', ctx => {
-    expect(ctx.stdout).to.contain('hello')
-  })
-
-  test
-  .stdout()
-  .do(() => cmd.run(['--help']))
-  .it('runs gtmt --help', ctx => {
-    console.log(ctx.stdout);
-    expect(ctx.stdout).to.contain('USAGE');
-  })
-
-})
+    .stdout()
+    .do(() => cmd.run([]))
+    .it("runs gtmt", (ctx) => {
+      expect(ctx.stdout).to.contain("hello");
+    });
+});
