fb747d5d48fa6ddcc5b86984eb202c0f749f0a58
yukimemi <yukimemi@gmail.com>
2020-05-10 23:57:40 +0900

    :zap: Get portfolio and save to json.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Command, flags } from "@oclif/command";
 import * as fs from "fs";
+import * as _ from "lodash";
+import * as moment from "moment";
 import * as puppeteer from "puppeteer";
 
 class Gtmt extends Command {
@@ -13,9 +15,11 @@ class Gtmt extends Command {
     force: flags.boolean({ char: "f" }),
   };
 
-  headless = false;
+  headless = true;
 
-  slowMo = 50;
+  slowMo = 0;
+
+  devtools = true;
 
   timeout = 0;
 
@@ -25,9 +29,55 @@ class Gtmt extends Command {
 
   cookiesPath = "cookies.json";
 
+  portfolioPath = "portfolio.json";
+
+  filterPortfolio = [
+    "time",
+    "住信SBIネット銀行",
+    "三井住友銀行",
+    "三菱UFJ銀行",
+    "ゆうちょ銀行",
+    "SBI証券",
+    "coincheck",
+    "bitbank",
+    "bitFlyer",
+    "BTCBOX",
+    "Liquid by Quoine",
+    "2681-ゲオHD",
+    "8267-イオン",
+    "One-MHAM新興成長株オープン",
+    "三井住友TAM-世界経済インデックスファンド",
+    "Amazon.co.jp-Amazonポイント",
+  ];
+
   browser!: puppeteer.Browser;
 
   page!: puppeteer.Page;
+
+  portfolio: object = { time: moment().format("YYYY/MM/DD HH:mm:ss.SSS") };
+
+  toNormalNumber(yen = ""): string {
+    if (/[^0-9,]*([0-9,]+)円/.test(yen)) {
+      return yen
+        .replace(/[^0-9,]*([0-9,]+)円/, "$1")
+        .replace(/,/g, "")
+        .replace(/\r?\n/g, "");
+    }
+    return yen;
+  }
+
+  getTableData(table: Element): object {
+    const thead = [
+      ...((table as HTMLTableElement).tHead?.rows[0] as HTMLTableRowElement)
+        .cells,
+    ].map((x) => x.innerText.trim());
+
+    const rows = [...(table as HTMLTableElement).tBodies[0].rows].map((row) =>
+      [...(row as HTMLTableRowElement).cells].map((x) => x.innerText.trim())
+    );
+
+    return rows.map((row) => _.zipObject(thead, row));
+  }
 
   async init(): Promise<void> {
     try {
@@ -35,9 +85,12 @@ class Gtmt extends Command {
       this.browser = await puppeteer.launch({
         headless: this.headless,
         slowMo: this.slowMo,
+        devtools: this.devtools,
         timeout: this.timeout,
       });
       this.page = await this.browser.newPage();
+      this.page.setDefaultTimeout(this.timeout);
+      this.page.on("console", (msg) => console.log("PAGE LOG: ", msg.text()));
     } finally {
       this.log("init end");
     }
@@ -60,11 +113,11 @@ class Gtmt extends Command {
       });
 
       await Promise.all([
-        this.page.waitForNavigation({ timeout: this.timeout }),
+        this.page.waitForNavigation(),
         this.page.click('a[href="/users/sign_in"]'),
       ]);
       await Promise.all([
-        this.page.waitForNavigation({ timeout: this.timeout }),
+        this.page.waitForNavigation(),
         this.page.click('a[href^="/sign_in/email"]'),
       ]);
 
@@ -73,7 +126,7 @@ class Gtmt extends Command {
         this.moneyforwardEmail
       );
       await Promise.all([
-        this.page.waitForNavigation({ timeout: this.timeout }),
+        this.page.waitForNavigation(),
         this.page.click('input[type="submit"]'),
       ]);
 
@@ -88,7 +141,7 @@ class Gtmt extends Command {
       });
 
       await Promise.all([
-        this.page.waitForNavigation({ timeout: this.timeout }),
+        this.page.waitForNavigation(),
         this.page.click('input[type="submit"]'),
       ]);
 
@@ -112,17 +165,148 @@ class Gtmt extends Command {
 
       await this.signin();
 
-      const cookies = await this.page.cookies();
-      fs.writeFileSync(this.cookiesPath, JSON.stringify(cookies));
+      await this.page.goto("https://moneyforward.com/bs/portfolio", {
+        waitUntil: ["load", "networkidle2"],
+      });
 
-      // const cookies = JSON.parse(fs.readFileSync(this.cookiesPath, "utf-8"));
-      // for (const cookie of cookies) {
-      //   await page.setCookie(cookie);
-      // }
+      await this.page.screenshot({
+        path: "screenshot/moneyforward_portfolio_1.png",
+        fullPage: true,
+      });
 
-      // await this.page.goto("https://moneyforward.com/bs/portfolio", {
-      //   waitUntil: ["load", "networkidle2"],
-      // });
+      // Summary
+      const summaryEl = await this.page.$("div.heading-radius-box");
+      const summaryStr = await summaryEl?.evaluate((x) => x.innerHTML);
+      const summary = [
+        {
+          種類: "資産総額",
+          残高: (summaryStr as string)
+            .replace(/[^0-9,]*([0-9,]+円)\n/, "$1")
+            .replace(/\r?\n/g, ""),
+          balance: this.toNormalNumber(summaryStr),
+        },
+      ];
+      console.log({ summary });
+
+      // Assets
+      const assetsOut = await this.page.$eval(
+        "section.bs-total-assets table tbody",
+        (tbody) =>
+          [...(tbody as HTMLTableElement).rows].map((row) => {
+            const kind = row.cells[0].innerText.trim();
+            const balance = row.cells[1].innerText.trim();
+            return { 種類: kind, 残高: balance };
+          })
+      );
+      const assets = _.map(assetsOut, (asset) => {
+        return { ...asset, balance: this.toNormalNumber(asset.残高) };
+      });
+      console.log({ assets });
+
+      // portfolio_det_depo
+      const detDepoOut = await this.page.$eval(
+        "section#portfolio_det_depo table",
+        this.getTableData
+      );
+      const detDepo = _.map(detDepoOut, (depo: object) => {
+        if (Object.hasOwnProperty.call(depo, "残高")) {
+          return {
+            ...depo,
+            balance: this.toNormalNumber((depo as { 残高: string }).残高),
+          };
+        }
+        return { ...depo, balance: 0 };
+      });
+      console.log({ detDepo });
+
+      // portfolio_det_eq
+      const detEqOut = await this.page.$eval(
+        "section#portfolio_det_eq table",
+        this.getTableData
+      );
+      const detEq = _.map(detEqOut, (eq: object) => {
+        if (Object.hasOwnProperty.call(eq, "評価額")) {
+          return {
+            ...eq,
+            balance: this.toNormalNumber((eq as { 評価額: string }).評価額),
+          };
+        }
+        return { ...eq, balance: 0 };
+      });
+      console.log({ detEq });
+
+      // portfolio_det_mf
+      const detMfOut = await this.page.$eval(
+        "section#portfolio_det_mf table",
+        this.getTableData
+      );
+      const detMf = _.map(detMfOut, (mf: object) => {
+        if (Object.hasOwnProperty.call(mf, "評価額")) {
+          return {
+            ...mf,
+            balance: this.toNormalNumber((mf as { 評価額: string }).評価額),
+          };
+        }
+        return { ...mf, balance: 0 };
+      });
+      console.log({ detMf });
+
+      // portfolio_det_fx
+      const detFxOut = await this.page.$eval(
+        "section#portfolio_det_fx table",
+        this.getTableData
+      );
+      const detFx = _.map(detFxOut, (fx: object) => {
+        if (Object.hasOwnProperty.call(fx, "残高")) {
+          return {
+            ...fx,
+            balance: this.toNormalNumber((fx as { 残高: string }).残高),
+          };
+        }
+        return { ...fx, balance: 0 };
+      });
+      console.log({ detFx });
+
+      // portfolio_det_po
+      const detPoOut = await this.page.$eval(
+        "section#portfolio_det_po table",
+        this.getTableData
+      );
+      const detPo = _.map(detPoOut, (po: object) => {
+        if (Object.hasOwnProperty.call(po, "現在の価値")) {
+          return {
+            ...po,
+            balance: this.toNormalNumber(
+              (po as { 現在の価値: string }).現在の価値
+            ),
+          };
+        }
+        return { ...po, balance: 0 };
+      });
+      console.log({ detPo });
+
+      this.portfolio = {
+        ...this.portfolio,
+        summary,
+        assets,
+        detDepo,
+        detEq,
+        detMf,
+        detFx,
+        detPo,
+      };
+
+      console.log({ portfolio: this.portfolio });
+
+      if (fs.existsSync(this.portfolioPath)) {
+        const portfolio = JSON.parse(
+          fs.readFileSync(this.portfolioPath, "utf-8")
+        );
+        const newPortfolio = _.concat(portfolio, this.portfolio);
+        fs.writeFileSync(this.portfolioPath, JSON.stringify(newPortfolio));
+      } else {
+        fs.writeFileSync(this.portfolioPath, JSON.stringify([this.portfolio]));
+      }
 
       await this.browser.close();
     } catch (error) {
