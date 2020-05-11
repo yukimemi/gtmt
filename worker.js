c90a8469748f40f1637299ea9b924b5e229bf353
yukimemi <yukimemi@gmail.com>
2020-05-12 01:45:03 +0900

    test

--- worker.js
+++ worker.js
@@ -1,8 +1,9 @@
 const CronJob = require("cron").CronJob;
 
 const workerJob = new CronJob({
-  cronTime: "0 6 * * *",
+  cronTime: "*/1 * * * *",
   onTick: function () {
+    console.log("onTick start !");
     const fs = require("fs");
     const path = require("path");
     const project = path.join(__dirname, "tsconfig.json");
@@ -15,8 +16,11 @@ const workerJob = new CronJob({
     require(`./${dev ? "src" : "lib"}`)
       .run()
       .catch(require("@oclif/errors/handle"));
+
+    console.log("onTick end !");
   },
   start: true,
   timeZone: "Asia/Tokyo",
 });
+console.log("worker start !");
 workerJob.start();
