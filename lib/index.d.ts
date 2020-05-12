e2e90a7ed2883b83bb4b0ade3855727a7679ee53
yukimemi <yukimemi@gmail.com>
2020-05-12 12:44:41 +0900

    :zap: Fix devDependencies and dependencies.

---
+++ lib/index.d.ts
@@ -0,0 +1,33 @@
+import { Command } from "@oclif/command";
+import * as puppeteer from "puppeteer";
+declare class Gtmt extends Command {
+    static description: string;
+    static flags: {
+        version: import("@oclif/parser/lib/flags").IBooleanFlag<void>;
+        help: import("@oclif/parser/lib/flags").IBooleanFlag<void>;
+        force: import("@oclif/parser/lib/flags").IBooleanFlag<boolean>;
+    };
+    headless: boolean;
+    slowMo: number;
+    devtools: boolean;
+    timeout: number;
+    moneyforwardEmail: string | undefined;
+    moneyforwardPass: string | undefined;
+    slackToken: string | undefined;
+    cookiesPath: string;
+    portfolioPath: string;
+    filterPortfolio: string[];
+    browser: puppeteer.Browser;
+    page: puppeteer.Page;
+    portfolio: object;
+    toNormalNumber(yen?: string): string;
+    getTableData(table: Element): object;
+    filterBalance(str: string): Promise<number[]>;
+    filterDetDepo(fil1: string, fil2: string): Promise<number[]>;
+    filterDetEq(fil1: string): Promise<number[]>;
+    filterDetMf(fil1: string): Promise<number[]>;
+    init(): Promise<void>;
+    signin(): Promise<void>;
+    run(): Promise<void>;
+}
+export = Gtmt;
--- worker.js
+++ worker.js
@@ -1,21 +1,14 @@
 const CronJob = require("cron").CronJob;
 
 const workerJob = new CronJob({
-  cronTime: "0 5 * * *",
+  // cronTime: "0 6 * * *",
+  cronTime: "*/1 * * * *",
   onTick: function () {
     console.log("onTick start !");
     const fs = require("fs");
     const path = require("path");
-    const project = path.join(__dirname, "tsconfig.json");
-    const dev = fs.existsSync(project);
 
-    if (dev) {
-      require("ts-node").register({ project });
-    }
-
-    require(`./${dev ? "src" : "lib"}`)
-      .run()
-      .catch(require("@oclif/errors/handle"));
+    require("./lib").run().catch(require("@oclif/errors/handle"));
 
     console.log("onTick end !");
   },
